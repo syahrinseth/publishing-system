ab9a3c21dfba1af5db3e1a4436b6ad7357d7e2de
syahrinseth <syahrinsethservices@gmail.com>
2022-05-24 09:04:14 +0800

    Inertia Share Prop Fix

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Request;
 use Inertia\Middleware;
+use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -37,7 +38,9 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
-            //
+            'auth.user' => fn () => auth()->user()
+                ? new UserResource(auth()->user())
+                : null,
         ]);
     }
 }
