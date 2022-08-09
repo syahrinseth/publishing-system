bc385c2b41e55edf61db47c82b8201795640efa4
syahrinseth <syahrinsethservices@gmail.com>
2022-08-10 02:22:37 +0800

    SMJS-27 enable publisher to submit to editor

--- resources/js/Pages/Manuscript/Edit.vue
+++ resources/js/Pages/Manuscript/Edit.vue
@@ -24,7 +24,7 @@
                     </div>
                     <div class="mt-5 flex lg:mt-0 lg:ml-4">
 
-                        <span class="sm:ml-3" v-if="(manuscript.data.status == `Draft`) && (viewAs == `author` || viewAs == `corresponding author`)">
+                        <span class="sm:ml-3" v-if="(manuscript.data.status == `Draft`) && (viewAs == `author` || viewAs == `corresponding author` || viewAs == `publisher`)">
                             <a href="#" @click="showSubmitToEditorModal = true" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500">
                                 <DocumentSearchIcon class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
                                 Submit To Editor
