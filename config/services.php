f04d38eb94846f399aaa7ed8277e326bf1db1adc
syahrinseth <syahrinsethservices@gmail.com>
2023-06-23 22:57:39 +0800

    add gtag

--- config/services.php
+++ config/services.php
@@ -30,4 +30,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'gtag' => [
+        'src' => env('GTAG_SRC'),
+        'key' => env('GTAG_KEY')
+    ]
+
 ];
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
+    @if(!empty(config('services.gtag.key')))
+      <!-- Google tag (gtag.js) -->
+      <script async src="{{ config('services.gtag.src') }}"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', `{{ config('services.gtag.key') }}`);
+    </script>
+    @endif
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
